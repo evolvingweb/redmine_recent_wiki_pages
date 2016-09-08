e21843eca82b4751ece3e50fa977a1cb7d6c3b01
Dave Vasilevsky <dave@vasilevsky.ca>
2016-09-08 00:13:50 -0400

    Update recent wiki pages; paginate

--- app/controllers/recent_wiki_pages_controller.rb
+++ app/controllers/recent_wiki_pages_controller.rb
@@ -1,11 +1,13 @@
 class RecentWikiPagesController < ApplicationController
+	def self.pages_with_updated_on
+		WikiPage.with_updated_on
+			.joins(:content)
+			.order("#{WikiContent.table_name}.updated_on DESC")
+	end
+
   def date_index
-    @pages = WikiPage.find :all, 
-      :select => "#{WikiPage.table_name}.*, #{WikiContent.table_name}.updated_on", 
-      :joins => "LEFT JOIN #{WikiContent.table_name} ON #{WikiContent.table_name}.page_id = #{WikiPage.table_name}.id", 
-      :order => "#{WikiContent.table_name}.updated_on DESC"
+    @paginator, @pages = paginate self.class.pages_with_updated_on
     @pages_by_date = @pages.group_by {|p| p.updated_on.to_date}
-    @pages_by_parent_id = @pages.group_by(&:parent_id)
 
     @pages.first.wiki
   end
